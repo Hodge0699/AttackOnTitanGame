476ce2103bca1a2619d513b6d35943e396447be3
Hodge0699 <39035064+Hodge0699@users.noreply.github.com>
2019-06-30 09:27:36 +0100

    Camera

---
+++ AOTGame/Assets/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    public bool LookAtPlayer = false;
+
+    public bool RotateAroundPlayer = true;
+
+    public float RotationsSpeed = 5.0f;
+
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    void LateUpdate()
+    {
+     if(RotateAroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
+
+            _cameraOffset = camTurnAngle * _cameraOffset;
+        }
+
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        if (LookAtPlayer || RotateAroundPlayer)
+            transform.LookAt(PlayerTransform);
+    }
+
+
+
+
+}
---
+++ AOTGame/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 18;
+
+    private Rigidbody rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();     
+    }
+
+    void Update()
+    {
+        float hAixs = Input.GetAxis("Horizontal");
+        float vAixs = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(hAixs, 0, vAixs) * speed * Time.deltaTime;
+
+
+        rig.MovePosition(transform.position + movement);
+    }
+}
