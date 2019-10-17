647a09be7dcb2776ccfb301319abd960dde19d9a
Hodge0699 <39035064+Hodge0699@users.noreply.github.com>
2019-10-17 12:07:30 +0100

    updated playerMovement

---
+++ AOTGame/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+
+    void Start()
+    {
+         
+    }
+    void Update()
+    {
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+
+}
